3d6dbd1b812bbdb1b3e8a03a5953105028dcf0e4
Mike Zamansky <zamansky@gmail.com>
2019-04-02 08:08:51 -0400

    Added lab

---
+++ test.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "funcs.h"
+
+#include "doctest.h"
+
+TEST_CASE("Sample tests"){
+  std::string result;
+  result=greet("Tom");
+  
+  CHECK(result.compare("Hello Tom!")==0);
+  
+}
+
+// Write test cases here
+TEST_CASE("Task A"){
+}
+
+TEST_CASE("Task B"){
+  
+}
+
+// etc.
+
+
+
