b01ebf88a68d8cfbde60c9947b5ff56d38b2c947
Chloe <chloe.gottlieb30@myhunter.cuny.edu>
2019-04-11 08:40:20 -0400

    First

--- funcs.cpp
+++ funcs.cpp
@@ -8,5 +8,18 @@ std::string greet(std::string name){
   return "Hello "+name+"!";
 }
 
+std::string remove_leading_spaces(string line){
+	int count = line.length;
+	string new_string ="";
+	for (int i = 0; i < count; ++i)
+	{
+		if (line[i] != ' ')
+		{
+			new_string = new_string + line[i];/* code */
+		}
+	}
+	return new_string;
+}
+
 // Write all your functions in here
 
--- main.cpp
+++ main.cpp
@@ -12,6 +12,8 @@ int main()
   result=greet("Tom");
   std::cout << result <<"\n";
   // Call the functions you wrote in funcs here
-  
+  std::string remove;
+  remove=removeLeadingSpaces("       int x = 1;  ");
+  std::cout << remove << "\n";
   return 0;
 }
